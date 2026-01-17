5f4039172eaaf6a285b7448308fef917b9ca6553
Thomas Bergheim <knighthk@glvortex.net>
2026-01-17 15:26:44 +0100

    Make an emacs cache worktree so an llm can't mess it up

--- start-emacs.sh
+++ start-emacs.sh
@@ -2,20 +2,44 @@
 
 USER_ID=$(id -u)
 USERNAME=$(whoami)
-
+WORKTREE_DIR="$HOME/.cache/aimacs-lyra"
+CONFIG_DIR="$HOME/.config/emacs"
 ENV_FILE=".env"
 ENV_ARGS=""
+
 if [ -f "$ENV_FILE" ]; then
     ENV_ARGS=$(xargs -a "$ENV_FILE" -I {} echo -n "-e {} ")
 fi
+
+if [ ! -d "$WORKTREE_DIR" ]; then
+    yadm worktree prune
+    yadm worktree remove "$WORKTREE_DIR"
+    yadm branch -D lyra-experiments
+    rm -rf "$WORKTREE_DIR"
+
+    yadm worktree add "$WORKTREE_DIR" -b lyra-experiments
+
+    cd "$WORKTREE_DIR/.config/emacs"
+    git init
+    git remote add origin "$CONFIG_DIR"
+    git fetch
+    git checkout -b main --track origin/master
+
+    # delete my secrets file, we don't need it
+    rm private.el
+fi
+
+mkdir -p "$HOME/.cache/emacs-lyra"
 
 podman run -it --rm \
     --name emacs-gui --userns keep-id \
     -e WAYLAND_DISPLAY \
     -e EMACS_CONTAINER=1 \
     -v "$XDG_RUNTIME_DIR:/tmp/runtime-$USER_ID:ro" \
-    -v "$HOME/.config/emacs:/home/$USERNAME/.config/emacs:Z" \
-    -v "$HOME/.cache/emacs:/home/$USERNAME/.cache/emacs:Z" \
+    -v "$WORKTREE_DIR/.config/emacs:/home/$USERNAME/.config/emacs:Z" \
+    -v "$HOME/.cache/emacs-lyra:/home/$USERNAME/.cache/emacs:Z" \
+    -v "$HOME/.local/share/yadm:/home/$USERNAME/.local/share/yadm:Z" \
+    -v "$HOME/llm:/home/$USERNAME/llm:Z" \
     --device /dev/dri \
     --security-opt label=disable \
     $ENV_ARGS \
